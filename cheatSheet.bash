7986ba932fda075035cc08a529a8734e59d162ca
feScholucha <felipescholucha@usp.br>
2025-06-23 15:19:52 -0300

    Merge Inicial - Entrega 1 O código ainda carece de uma coesão boa, algumas partes estão quebradas e inumeras outras faltam comentários, estou trabalhando nisso atualmente

---
+++ cheatSheet.bash
@@ -0,0 +1,7 @@
+# Estou usando o python 3.11.12 para os testes atuais
+# Se os comandos em bash não rodarem automáticamente, execute manualmente
+
+virtualenv .venv --python=python3.11
+source .venv/bin/activate
+pip install -r requirements.txt
+# Vai demorar, boa sorte
